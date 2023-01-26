b40a0f6d6d5ee4bb81089e29503850ae2c9a7d3b
Riley Snyder <rileysnyder@protonmail.ch>
2023-01-26 08:08:36 -0600

    Update main.tf

--- blank_tf/main.tf
+++ blank_tf/main.tf
@@ -1,5 +1,5 @@
-resource "null_resource" "this" {
-  provisioner "local-exec" {
-    command = "pwd"
-  }
-}
+# resource "null_resource" "this" {
+#   provisioner "local-exec" {
+#     command = "pwd"
+#   }
+# }
